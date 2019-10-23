a10f2f72ca65a305c92f5d04e26a945fe925627c
Kaxx03 <54406936+Kaxx03@users.noreply.github.com>
2019-10-23 17:08:07 -0500

    Add files via upload

---
+++ server.py
@@ -0,0 +1,15 @@
+from flask import Flask, jsonify, request
+from flask_cors import CORS
+from api.controllers.TipoCultivos import TipoCultivo
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route('/tipoCultivos', methods = ['GET'])
+def getAll():
+    return (TipoCultivo.list())
+
+@app.route('/tipoCultivos', methods = ['POST'])
+def postOne():
+    body = request.json
+    return (TipoCultivo.create(body))
